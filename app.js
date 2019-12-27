ca4c70abc3d31dd3ea6d472d146fc9cea39ff1f7
Akronborn <freewillandjustice@gmail.com>
2019-12-27 01:36:42 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const gTTS = require('gtts');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+app.post('/', (req, res) => {
+
+  let text = req.body.text;
+  let filename = req.body.filename;
+  let gtts = new gTTS(text, 'en');
+  gtts.save(`${filename}.mp3`, function (err, result) {
+    if (err) { throw new Error(err) }
+    console.log('Audio file created.');
+  });
+})
+
+
+app.listen(3000, () =>
+  console.log(`Server is listening ${port}`)
+);
