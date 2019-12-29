b91af76da45467adf6fb516cba2935a67f06bdf5
Akronborn <freewillandjustice@gmail.com>
2019-12-29 15:20:34 -0500

    make it better

--- app.js
+++ app.js
@@ -23,9 +23,10 @@ app.post('/', (req, res) => {
   let gtts = new gTTS(text, `${lang}`);
   gtts.save(`${filename}.mp3`, function (err, result) {
     if (err) { throw new Error(err) }
+    //let filePath = path.join(__dirname, `/downloads/${filename}.mp3`)
     res.download(`${filename}.mp3`);
     console.log('Audio file created.');
-    res.redirect('/');
+    //res.redirect('/');
   });
 })
 
